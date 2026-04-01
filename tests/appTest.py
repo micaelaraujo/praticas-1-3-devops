9ca5f0bf5d0bba237051dabcc8880e98c7eb472f
Micael A. Santos <81994360+micaelaraujo@users.noreply.github.com>
2026-04-01 01:38:05 +0000

    Aumenta cobertura de teste

--- tests/appTest.py
+++ tests/appTest.py
@@ -10,3 +10,13 @@ class AppTest(unittest.TestCase):
         self.assertEqual(200, response.status_code, "Erro no test_http_code!")
         self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
                           , "Erro no test_print_health_check!")
+
+    def test_print_hello_success(self):
+        response = self.app.get("/hello", query_string={"name": "Micael"})
+        self.assertEqual(200, response.status_code, "Erro no test_print_hello_success!")
+        self.assertEqual(f"Hello, Micael!", response.get_data(as_text=True), "Erro no test_print_hello_success!")
+
+    def test_print_hello_error(self):
+        response = self.app.get("/hello")
+        self.assertEqual(400, response.status_code, "Erro no test_print_hello_error!")
+        self.assertEqual("Nome não informado", response.get_data(as_text=True), "Erro no test_print_hello_success!")
